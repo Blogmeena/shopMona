c9216b588723c4055d2722453780a588f257d48e
Meena gupta <meenagarg1988@gmail.com>
2024-06-21 19:18:35 +0530

    model and admin added

--- shopMona/app/admin.py
+++ shopMona/app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Customer,
+    Cart,
+    OrderPlaced,
+    Product
+)
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','name','locality','city','zipcode','state']
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title','selling_price','discounted_price','description','brand','category','product_image']   
+
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','Product','quantity']
+
+@admin.register(OrderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','Customer','Product','quantity','ordered_date','status']
